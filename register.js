1ed10e62e39374175d76b8867fdb96783651b1c6
notzilly <imnotzilly@gmail.com>
2023-04-01 02:46:55 -0300

    fix: register all commands from commands folder automatically

--- register.js
+++ register.js
@@ -1,27 +1,33 @@
 require('dotenv').config();
 const { REST, Routes } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
 
 const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
-const commands = [
-    {
-        name: 'ping',
-        description: 'Replies with Pong!',
-    },
-    {
-        name: 'kurisu',
-        description: 'tina!',
-    }
-];
+const commands = [];
+// Grab all the command files from the commands directory
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	commands.push(command.data.toJSON());
+}
 
 (async () => {
-    try {
-        console.log('Started refreshing application (/) commands.');
+	try {
+		console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
-        await rest.put(Routes.applicationCommands(process.env.APP_ID), { body: commands });
+		// Refresh commands
+		const data = await rest.put(
+			Routes.applicationCommands(process.env.APP_ID),
+			{ body: commands },
+		);
 
-        console.log('Successfully reloaded application (/) commands.');
-    } catch (error) {
-        console.error(error);
-    }
+		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+	} catch (error) {
+		console.error(error);
+	}
 })();
