6fc6a12665e9f0db8d38a7863aaed454eb1d308c
notzilly <imnotzilly@gmail.com>
2023-04-01 02:47:21 -0300

    fix: bot loads commands from commands folder and replies accordingly

--- bot.js
+++ bot.js
@@ -1,28 +1,49 @@
 require('dotenv').config();
-const { Client, GatewayIntentBits } = require('discord.js');
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const { Client, GatewayIntentBits, Events, Collection } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+// Load up Amadeus
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+client.commands = new Collection();
+
+// Fetch all commands inside `commands` folder
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	// Set a new item in the Collection with the key as the command name and the value as the exported module
+	if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+	} else {
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	}
+}
+
+// Login event => Amadeus ON
+client.on(Events.ClientReady, c => {
+  console.log(`Logged in as ${c.user.tag}!`);
 });
 
-client.on('interactionCreate', async interaction => {
+// All chat interactions go through here
+client.on(Events.InteractionCreate, async interaction => {
   if (!interaction.isChatInputCommand()) return;
 
-  if (interaction.commandName === 'ping') {
-    await interaction.reply('Pong!');
-  }
+  const command = interaction.client.commands.get(interaction.commandName);
 
-  const kurisuEmbed = {
-    title: '~TINA MO KINSHII!',
-    image: {
-        url: 'http://i.imgur.com/vtNaFDQ.gif',
-    }
-  }
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
 
-  if (interaction.commandName === 'kurisu') {
-    await interaction.reply({ embeds: [kurisuEmbed]})
-  }
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(`Error executing ${interaction.commandName}`);
+		console.error(error);
+	}
 });
 
 client.login(process.env.TOKEN);
